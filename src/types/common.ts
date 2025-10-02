c4e89e9fffebb8af981e6de611f2eb3a7660d2c8
David Choloyan <shamsani.david@gmail.com>
2025-10-02 19:37:38 +0200

    feat: unneded types

--- src/types/common.ts
+++
@@ -1,5 +0,0 @@
-export type VariantProps<T extends (...args: any) => any> = T extends (props?: infer P) => any
-  ? P extends { className?: string }
-    ? Omit<P, "className">
-    : P
-  : never;
--- src/types/pagination.ts
+++
@@ -1,7 +0,0 @@
-import type { VariantProps } from "./common";
-
-import { buttonVariants } from "@/constants/buttonVariants";
-
-export interface PaginationProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
