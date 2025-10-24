f172c46917f43941deacef26ec38e4e6bcd59daa
David Choloyan <shamsani.david@gmail.com>
2025-10-24 16:43:29 +0200

    fix: unneded external in vite

--- vite.config.ts
+++ vite.config.ts
@@ -61,9 +61,6 @@ export default defineConfig({
     rollupOptions: {
       external: [
         "react/jsx-runtime",
-        "@tanstack/router-core",
-        "@tanstack/react-store",
-        "@tanstack/history",
         ...Object.keys(packageJson.peerDependencies),
       ],
       output: [
